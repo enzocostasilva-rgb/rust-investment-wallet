a3a365ac91f0a1d5e66534899ae7d33d2706c0f1
enzocostasilva-rgb <enzocostasilva@hotmail.com>
2026-08-28 20:28:12 -0300

    Update bitcoin_asset.sql

--- src/routes/fixtures/bitcoin_asset.sql
+++ src/routes/fixtures/bitcoin_asset.sql
@@ -1,2 +1,14 @@
-INSERT INTO assets (name, ticker, asset_type, quantity, unit_value)
-VALUES ('Bitcoin', 'BTC', 'CRYPTO', 0.5, 100000.0);
+INSERT INTO assets (
+    name,
+    ticker,
+    asset_type,
+    quantity,
+    unit_value
+)
+VALUES (
+    'Bitcoin',
+    'BTC',
+    'CRYPTO',
+    0.5,
+    100000.0
+);
