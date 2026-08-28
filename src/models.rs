3246dd457cbe2115ce934a76aed5686ca945d687
enzocostasilva-rgb <enzocostasilva@hotmail.com>
2026-08-28 19:29:46 -0300

    Update models.rs

--- src/models.rs
+++ src/models.rs
@@ -4,6 +4,9 @@ use serde::Serialize;
 pub struct Asset {
     pub id: i64,
     pub name: String,
+    pub ticker: String,
+    pub asset_type: String,
+    pub quantity: f64,
     pub unit_value: f64,
 }
 
